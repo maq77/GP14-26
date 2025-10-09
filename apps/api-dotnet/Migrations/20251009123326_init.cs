a218c1c138fce4177efd79f791f638e962e6dbd2
Mohamed Amin <40144153+maq77@users.noreply.github.com>
2025-10-09 16:26:59 +0300

    env-ready
    Development-Ready-Repo

---
+++ apps/api-dotnet/Migrations/20251009123326_init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apidotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Incidents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Severity = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    PayloadJson = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidents", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Incidents");
+        }
+    }
+}
---
+++ apps/web-dashboard/web-dashboard/vite.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  root: "web-dashboard",                 // <- inner folder that has index.html + src/
+  plugins: [react()],
+  build: { outDir: "../dist", emptyOutDir: true },  // emit dist next to package.json
+  server: { port: 5173 }
+});
