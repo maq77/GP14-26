7c2389d9827a39193c4cd0c80eff52309e51dfd0
Mohamed Amin <40144153+maq77@users.noreply.github.com>
2025-11-30 13:29:55 +0200

    added-face-embedding-.net

---
+++ apps/api/src/SSSP.DAL/Models/FaceEmbedding.cs
@@ -0,0 +1,19 @@
+﻿using SSSP.DAL.Abstractions;
+
+namespace SSSP.DAL.Models
+{
+    public class FaceEmbedding : IEntity<int>
+    {
+        public int Id { get; set; }
+
+        // Navugation to FaceProfile
+        public Guid FaceProfileId { get; set; }
+        public FaceProfile FaceProfile { get; set; } = null!;
+
+        public byte[] Vector { get; set; } = Array.Empty<byte>();
+
+        public string? SourceCameraId { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
